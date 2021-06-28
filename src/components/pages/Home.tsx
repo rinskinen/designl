39a2e93df84b02ce5fb68e2db22435300686866c
rinskinen <riina.j.moilanen@gmail.com>
2021-06-28 19:27:23 +0300

    Refactor front page. Add pics.

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,19 +6,66 @@ const StyledHomeDiv = styled.div`
   display: grid;
   align-content: center;
   justify-content: stretch;
-  p: 3px;
   text-align: center;
+`;
+
+const StyledImage = styled.img`
+  align-self: center;
+  display: grid;
+  align-content: center;
+  justify-content: center;
+
+  @media (min-width: 320px) {
+    padding-right: 20px;
+    margin-bottom: 10px;
+  }
+
+  @media (min-width: 1200px) {
+    padding-right: 50px;
+    margin-bottom: 50px;
+  }
+`;
+
+const StyledImageRow = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  @media (min-width: 320px) {
+    padding-right: 20px;
+    margin-bottom: 10px;
+  }
+
+  @media (min-width: 1200px) {
+    padding-right: 50px;
+    margin-bottom: 50px;
+  }
 `;
 
 export const Home = () => {
   return (
     <StyledHomeDiv>
+      <StyledImage
+        src="https://webdisk.designlatvalahti.fi/public_html/designlatvalahti/images/sensu/TaloSensu2.jpg"
+        alt="Talo Sensu"
+        width="100%"
+      />
+      <StyledImageRow>
+        <StyledImage
+          src="https://webdisk.designlatvalahti.fi/public_html/designlatvalahti/images/sensu/TaloSensu10.jpg"
+          alt="Talo Sensu"
+          width="50%"
+        />
+        <StyledImage
+          src="https://webdisk.designlatvalahti.fi/public_html/designlatvalahti/images/sensu/TaloSensu11.jpg"
+          alt="Talo Sensu"
+          width="50%"
+        />
+      </StyledImageRow>
       <Banner text="Sisustussuunnittelua yksityiskoteihin ja julkitiloihin." />
-      <Banner text="Esteettisiä, toimivia ja ajattomia tiloja, joissa on hyvä olla." />
-      <Banner
-        text="Kuvia kohteistani tulossa pian! Voit tutustua kohteisiini Instagram-tililläni "
-        link="https://www.instagram.com/casalautaajabetonia/?hl=fi"
-        linkText="@casalautaajabetonia"
+      <StyledImage
+        src="https://webdisk.designlatvalahti.fi/public_html/designlatvalahti/images/LatvalahtiDima2.jpg"
+        alt="Dima Latvalahti"
+        width="100%"
       />
     </StyledHomeDiv>
   );
