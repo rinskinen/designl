070499ecbdca659e546bf130f29681017fb2910e
rinskinen <riina.j.moilanen@gmail.com>
2021-06-28 19:36:39 +0300

    Add picture to About page

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import { StyledLine } from './pages/Services';
+
+const StyledContent = styled.div`
+  display: flex;
+
+  @media (min-width: 320px) {
+    flex-direction: column;
+  }
+
+  @media (min-width: 1200px) {
+    flex-direction: row;
+  }
+`;
 
 const StyledCard = styled.div`
   align-self: center;
   display: grid;
   align-content: center;
   justify-content: center;
-  border: 1.5px solid black;
   font-size: 24px;
   @media (min-width: 320px) {
     padding: 20px;
@@ -15,7 +25,7 @@ const StyledCard = styled.div`
   }
 
   @media (min-width: 1200px) {
-    padding: 50px;
+    padding: 0 50px 50px 50px;
     margin-bottom: 50px;
   }
 `;
@@ -25,37 +35,62 @@ const StyledHeader = styled.h1`
   text-align: center;
 `;
 
+const StyledImage = styled.img`
+  align-self: center;
+  display: grid;
+  align-content: center;
+  justify-content: center;
+
+  @media (min-width: 320px) {
+    padding-right: 20px;
+    margin-bottom: 10px;
+  }
+
+  @media (min-width: 1200px) {
+    padding-right: 50px;
+    margin-bottom: 50px;
+  }
+`;
+
 export const AboutContent = () => {
   return (
-    <StyledCard>
-      <StyledHeader>Tervetuloa sivuilleni!</StyledHeader>
-      <StyledLine />
-      <p>
-        Olen Dima Latvalahti ja valmistuin huhtikuussa 2021
-        sisustussuunnittelijaksi Helsinki Design Schoolista.
-      </p>
-      <p>
-        Ryhdyin yrittäjäksi tammikuussa 2021 ja perustin
-        sisustussuunnittelutoimiston.
-      </p>
-      <p>Suunnittelutyöni lähtökohta on aina asiakkaani kuuntelu.</p>
-      <p>
-        Moni kysyy minulta minkälainen sisustustyyli minulla on.
-        Kerron, mutta se ei kerro muusta kuin mistä minä pidän. Mutta
-        työssäni on kyse siitä miten saan tehtyä asiakkaalleni hänen
-        toiveidensa ja tarpeittensa mukainen koti.
-      </p>
-      <p>
-        Tarkoitus ei ole tehdä kohteesta itselleni käyntikorttia.
-        Asiakkaalla on usein paljon toiveita ja ideoita mitä hän
-        kotiinsa haluaa. Minun tehtävä on suunnittella näiden
-        perusteella kaunis yhtenäinen kokonaisuus, joka palvelee
-        kaikkia asiakkaan tarpeita.
-      </p>
-      <p>
-        Jokainen koti on asujansa henkilökohtainen maailmansa; sen
-        pitää olla myös asujansa näköinen.
-      </p>
-    </StyledCard>
+    <StyledContent>
+      <StyledImage
+        src="https://webdisk.designlatvalahti.fi/public_html/designlatvalahti/images/LatvalahtiDima.jpg"
+        alt="Dima Latvalahti"
+        width="50%"
+      />
+      <StyledCard>
+        <StyledHeader>Tervetuloa sivuilleni!</StyledHeader>
+        <p>
+          Olen Dima Latvalahti ja valmistuin huhtikuussa 2021
+          sisustussuunnittelijaksi Helsinki Design Schoolista.
+        </p>
+        <p>
+          Ryhdyin yrittäjäksi tammikuussa 2021 ja perustin
+          sisustussuunnittelutoimiston.
+        </p>
+        <p>
+          Suunnittelutyöni lähtökohta on aina asiakkaani kuuntelu.
+        </p>
+        <p>
+          Moni kysyy minulta minkälainen sisustustyyli minulla on.
+          Kerron, mutta se ei kerro muusta kuin mistä minä pidän.
+          Mutta työssäni on kyse siitä miten saan tehtyä asiakkaalleni
+          hänen toiveidensa ja tarpeittensa mukainen koti.
+        </p>
+        <p>
+          Tarkoitus ei ole tehdä kohteesta itselleni käyntikorttia.
+          Asiakkaalla on usein paljon toiveita ja ideoita mitä hän
+          kotiinsa haluaa. Minun tehtävä on suunnittella näiden
+          perusteella kaunis yhtenäinen kokonaisuus, joka palvelee
+          kaikkia asiakkaan tarpeita.
+        </p>
+        <p>
+          Jokainen koti on asujansa henkilökohtainen maailmansa; sen
+          pitää olla myös asujansa näköinen.
+        </p>
+      </StyledCard>
+    </StyledContent>
   );
 };
