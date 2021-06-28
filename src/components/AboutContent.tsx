979ce3b4f70ccf8badd5ee4b25af88f366f206f5
rinskinen <riina.j.moilanen@gmail.com>
2021-06-28 21:38:31 +0300

    Change image

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import image1 from '../images/LatvalahtiDima07.jpg';
+import image1 from '../images/LatvalahtiDima28.jpg';
 
 const StyledContent = styled.div`
   display: flex;
